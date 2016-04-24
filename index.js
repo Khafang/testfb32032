4702fe883d9984ddbbf16ab1e59c11b914cc88af
Khafang <jinnowat_loc@hotmail.com>
2016-04-24 16:13:07 +0700

    fix code

--- index.js
+++ index.js
@@ -67,19 +67,7 @@ app.post('/webhook/', function (req, res) {
 
          var plantext = "https://www.google.co.th/search?q=";
          plantext+=text;
-         $.ajax({
-           headers: { 'X-Auth-Token': '553e5ea5b09c4bb18bc65855b3cbb8f5' },
-           url: 'http://api.football-data.org/v1/fixtures?timeFrame=n1',
-           dataType: 'json',
-           type: 'GET',
-         }).done(function(response) {
-           // do something with the response, e.g. isolate the id of a linked resource
-           var regex = /.*?(\d+)$/; // the ? makes the first part non-greedy
-           var res = regex.exec(response.fixtures[0]._links.awayTeam.href);
-           var teamId = res[1];
-           console.log(teamId);
-         });
-         sendTextMessage(sender, teamId);
+         sendTextMessage(sender, plantext);
          flag = false;
        }
       else
