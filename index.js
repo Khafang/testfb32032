72b23999ed1f000170f9e7555821980810436aa4
Khafang <jinnowat_loc@hotmail.com>
2016-04-24 14:37:06 +0700

    add BodyParser

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 var express = require('express')
+var request = require('request')
+var bodyParser = require('body-parser')
+
 var app = express()
+app.use(bodyParser.json())
+var token = "CAACrY5Wxe4QBAJHXMBG2tDGRndlz9ZB7ZAHVOPG7i1H9i4FcIr0M5nWhVqhFXPTm8Q32zSQKyRPU2PyXNFIHr0TbZC3KqkilYS5BUQJ9d5rUpqLfveWcLKVSaPIA08ZCeYM5GZBo28iEP3qedWp1rFKPjsGM7KZAZAqFvT3Exg6RRVGVFqo5mDBRkGB4dlOk3QZD";
+
+function sendTextMessage(sender, text) {
+  messageData = {
+    text:text
+  }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
+
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
@@ -11,6 +38,20 @@ app.get('/webhook/', function (req, res) {
   }
   res.send('Error, wrong validation token');
 })
+
+app.post('/webhook/', function (req, res) {
+  messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+      sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
+    }
+  }
+  res.sendStatus(200);
+});
+
 
 app.set('port', (process.env.PORT || 5000))
 
