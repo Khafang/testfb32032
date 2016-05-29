1043f6255549135a6b1d66d342b90d30e2649096
Khafang <jinnowat_loc@hotmail.com>
2016-05-29 10:22:58 +0700

    Fix

--- index.js
+++ index.js
@@ -70,17 +70,7 @@ app.post('/webhook/', function (req, res) {
           else
           sendTextMessage(sender,var2);
           }
-      else  if(text.indexOf("avg") > -1){
-              fulltext = text.replace(/\s+/g, '');
-              var n = 3,result=0,count=0;
-            /*  while(fulltext.charAt(n) != ' '){
-                result += parseInt(fulltext.charAt(n));
-                n++;
-                count++;
-              }
-              result /= count;
-                sendTextMessage(sender,result);
-              }*/
+
       else
       sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
     }
